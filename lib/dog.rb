1dcdcc15bcb540bcf1fbf2c4fde25a23967b6b06
Hrudesh Lenka <hrudesh2010@gmail.com>
2020-06-27 07:47:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+def Dog
+  attr_accessor :name, :bark
+  def name
+    @name
+  end
+  def name=(name)
+    @name = name
+  end
+  def bark
+    @bark
+  end
+  def bark=(bark)
+    @bark = bark
+  end
+end
